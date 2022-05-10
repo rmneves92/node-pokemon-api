091053cfe910202172746b03af5bda4b6fbdd58a
Rafael Neves <rafa.mneves@hotmail.com>
2022-05-09 22:41:26 -0300

    return json response

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello, Express! 😄'));
 
 app.get('/api/pokemons/:id', (req, res) => {
-  const id = req.params.id;
-  console.log('ID: ', id);
-  const pokemon = pokemons.find(pokemon => pokemon.id === +id);
+  const id = +req.params.id;
+  const pokemon = pokemons.find(pokemon => pokemon.id === id);
 
-  res.send(`Vous avez demandé le pokémon ${pokemon.name}.`);
+  res.json(pokemon);
+});
+
+app.get('/api/pokemons', (req, res) => {
+  res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment.`);
 });
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur: http://localhost:${port}`));

797f8c2047d602fd28059b50ff24a160440e9cd6
Rafael Neves <rafa.mneves@hotmail.com>
2022-05-12 16:19:20 -0300

    add favicon

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
 const { success } = require('./helper');
 const pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
 
-app.use(morgan('dev'));
+app.use(favicon(__dirname + '/favicon.ico')).use(morgan('dev'));
 
 app.get('/', (req, res) => res.send('Hello, Express! 😄'));
 
